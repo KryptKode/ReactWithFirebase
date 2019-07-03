03e0c295598cd74b2264b05e1bf654e59200d22b
KryptKode <paulakhamiogu@gmail.com>
2019-07-03 22:39:20 +0100

    Created sign up page

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
+import {compose} from 'recompose';
+
+import { SignUpLink } from '../SignUp';
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes'; 
 
 const SignIn  = () =>(
     <div>
