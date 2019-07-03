bd40cde0ddf16402edfdb72af9940ef7c70f84f9
KryptKode <paulakhamiogu@gmail.com>
2019-07-03 22:50:45 +0100

    Created sign out functionality

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 
 const Navigation = () => {
@@ -22,9 +23,13 @@ const Navigation = () => {
                 <li>
                     <Link to={ROUTES.ACCOUNT}>Account</Link>
                 </li>
-                
+
                 <li>
                     <Link to={ROUTES.ADMIN}>Admin</Link>
+                </li>
+
+                <li>
+                    <SignOutButton />
                 </li>
             </ul>
         </div>
